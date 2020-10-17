39f32dd137d9bb47e113b09ea73bd6f5dcbb08d1
noahvolson <noah.v.olson@gmail.com>
2020-10-17 18:37:56 -0400

    Send login data from client to server

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
 function main() {
-    console.log("Hello World!")
+    console.log("Hello World!");
+    document.getElementById("loginBtn").onclick = login;
+}
+
+function login() {
+    console.log("Log in requested");
+    return fetch("/login", {
+        method:"POST",
+        body:JSON.stringify({
+            username: document.getElementById("usernameInput").value,
+            password: document.getElementById("passwordInput").value
+        }),
+        headers: { "Content-Type": "application/json"}
+    })
+        .then(response => response.json())
+        .then(json => {
+            return json
+        })
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const favicon = require("serve-favicon");
+const {MongoClient} = require('mongodb');
+const bodyParser = require("body-parser");
 
 
 /////////////////////////////////// Middleware ///////////////////////////////////
@@ -13,13 +15,33 @@ app.use(express.static("public"));
 
 
 /////////////////////////////////// Routes ///////////////////////////////////
-app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/views/index.html");
-});
-app.get("/index.html", (request, response) => {
-    response.sendFile(__dirname + "/views/index.html");
-});
 
+app.get('/', bodyParser.json(), (req, res) => {
+
+    if (req.user !== undefined && req.user !== null) { // if user has logged in
+        // send user data back
+        res.sendFile(__dirname + "/views/index.html");
+    }
+    else {
+        res.sendFile(__dirname + "/views/login.html");
+    }
+})
+
+app.get('/index.html', bodyParser.json(), (req, res) => {
+
+    if (req.user !== undefined && req.user !== null) { // if user has logged in
+        // send user data back
+        res.sendFile(__dirname + "/views/index.html");
+    }
+    else {
+        res.sendFile(__dirname + "/views/login.html");
+    }
+})
+
+app.post('/login', bodyParser.json(), (req, res) => {
+    console.log(req.body)
+    //req["user"] = 5
+})
 
 /////////////////////////////////// Listener  ///////////////////////////////////
 
@@ -30,3 +52,25 @@ const listener = app.listen(3000, () => {
 
 
 /////////////////////////////////// Database ///////////////////////////////////
+let DBclient = null;
+
+async function initConnection() {
+    const uri = `mongodb+srv://noahvolson:${process.env.DB_PASSWORD}@cluster0.mbhva.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+    DBclient = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+    await DBclient.connect();
+}
+
+async function getUser(username) {
+    if (DBclient === null) {await initConnection()}
+    let collection = DBclient.db("WebAuth").collection("UserData");
+    return await collection.findOne({username: username});
+}
+
+async function upsertUser(userData) {
+    if (DBclient === null) {await initConnection()}
+    let collection = DBclient.db("WebAuth").collection("UserData");
+    collection.updateOne(
+        { username: userData.username },
+        { $set: userData },
+        { upsert: true });
+}
